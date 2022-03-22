c25a3c41109a840c72a619cae53b13788541f7e0
lynchm18 <lynchm18@tcd.ie>
2022-03-22 14:36:57 +0000

    {#test-backdrop-corrected}

--- chase/code.py
+++ chase/code.py
@@ -3,3 +3,7 @@ import pytch
 
 class Sky(pytch.Stage):
     Backdrops = ["clouds.jpg"]
+    @pytch.when_green_flag_clicked
+    def checkRender(self):
+        assert self.Backdrops == ["clouds.jpg"], "Background Image not defined Properly"
+        print("Background Rendered successfully!")
